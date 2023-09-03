eb753b84585d8331b7339a32a05840439cf5d1ee
Darya Shelever <d.shelever@gmail.com>
2023-09-03 18:25:02 +0300

    UserController

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { error } from 'console'
+import { error, log } from 'console'
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
-import {validationResult} from 'express-validator'
-
 import {registerValidation} from './validations/auth.js'
+import checkAuth from './utils/checkAuth.js'
+import {register, login, get} from './controllers/UserController.js'
 
 mongoose.connect(
     'mongodb+srv://dshelever:Abc123@cluster0.ftj7c9a.mongodb.net/blog?retryWrites=true&w=majority')
@@ -16,43 +14,14 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register',registerValidation, async (req, res)=>{
-   try {
-    const error= validationResult(req) 
-    if(!error.isEmpty()){
-        return res.status(400).json(errors.array())
+app.post('/auth/login',)
+
+app.post('/auth/register',registerValidation, )
+
+app.get('/auth/me',checkAuth, )
+app.listen(4444, (err)=> {
+    if(err){
+        return console.log(err);
     }
-
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-        email:req.body.email,
-        fullName:req.body.fullName,
-        avatarUrl:req.body.avatarUrl,
-        passwordHash:hash,
-    })
-
-    const user= await doc.save()
-
-    const token = jwt.sign({
-       _id:user._id 
-    },
-    'secret123',
-    {
-        expiresIn:'30d'
-    })
-
-    const {passwordHash, ...userData}= user._doc
-    res.json({
-        ...user._doc,
-        token
-    })
-   } catch (error) {
-    console.log(error)
-    res.status(500).json({
-        message: 'failed to register', 
-    })
-   }
+    console.log('Server ok');
 })
