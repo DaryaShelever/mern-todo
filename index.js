ba2f119c2cbd8280dc5b97639c23338cc8ca1a74
Darya Shelever <d.shelever@gmail.com>
2023-09-03 18:27:18 +0300

    UserController

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import {registerValidation} from './validations/auth.js'
 import checkAuth from './utils/checkAuth.js'
-import {register, login, get} from './controllers/UserController.js'
+import * as UserController from './controllers/UserController.js'
 
 mongoose.connect(
     'mongodb+srv://dshelever:Abc123@cluster0.ftj7c9a.mongodb.net/blog?retryWrites=true&w=majority')
@@ -14,11 +14,12 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/login',)
+app.post('/auth/login', UserController.login)
 
-app.post('/auth/register',registerValidation, )
+app.post('/auth/register',registerValidation,UserController.register )
 
-app.get('/auth/me',checkAuth, )
+app.get('/auth/me',checkAuth,UserController.getMe )
+
 app.listen(4444, (err)=> {
     if(err){
         return console.log(err);
